3aedbe7f16d862d3f9574ee88b05b3b23bcfa45f
kenna-hwa <76397046+kenna-hwa@users.noreply.github.com>
2021-07-31 21:33:26 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({fav}){
+  return <h1>I like {fav} </h1>;
+}
 
 function App() {
+  // return <div className="App" /> ;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+  
+  <div>
+    <h1>HELLO</h1>
+    <Food fav="kimchi" />
+    <Food fav="ramen" />
+    <Food fav="samgiopsal" />
+    <Food fav="chukumi" />
+  </div>
+    
+  
+  
   );
 }
 
