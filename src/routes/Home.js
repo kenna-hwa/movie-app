b69d7d8dda12d892b64fd99f2d4e8fb85f8722de
kenna-hwa <76397046+kenna-hwa@users.noreply.github.com>
2021-08-09 18:01:52 +0900

    다시 원본 복귀

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -13,30 +13,12 @@ class Home extends React.Component {
   }
 
   getMovies = async () => {
-    const ID_KEY = 'UZV5oZ3BtdPJzOWTXH3E';
-    const SECRET_KEY = 'oa76uk3qhY';
-    const search = this.state.value;
-    try{
-      if(search === ""){
-        this.setState({movies:[], isLoading: false})
-      } else {
-        const {data: {
-          items
-        }} = await axios.get('https://openapi.naver.com/v1/search/movie.json', {
-          params: {
-            query: search,
-            display: 20
-          },
-          headers: {
-            'X-Naver-Client-Id': ID_KEY,
-            'X-Naver-Client-Secret': SECRET_KEY 
-          }
-        });
-        this.setState({movies: items, isLoading: false});
-      }
-    } catch(error) {
-      console.log(error);
-    }
+    const{
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('http://yts-proxy.now.sh/list_movies.json');
+    this.setState({ movies, isLoading: false });
   };
 
  componentDidMount(){
